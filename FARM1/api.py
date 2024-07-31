9585c6fff25410fc0b61cb5bab90901cd16895b9
Arthur Lopes <arthurlopes.ti@gmail.com>
2024-07-30 21:23:33 -0300

    atualizacao

--- FARM1/api.py
+++ FARM1/api.py
@@ -3,19 +3,18 @@ from routes.Jogador import jogador_router
 from fastapi.middleware.cors import CORSMiddleware
 
 client_app = [
-    "https://localhost:3000" #porta padrão que o react é inicializado
+    "http://localhost:3000" #porta padrão que o react é inicializado
 ]
 
 app = FastAPI()
+
+app.include_router(jogador_router)
+
 #Os clientes que estão permitido o acesso
 app.add_middleware(
     CORSMiddleware,
     allow_origins=client_app,
     allow_credentials=True,
     allow_methods=["*"], #acesso a todos os metodos
-    allow_headers=["*"] # Acesso a todos os cabeçalhos
-)
-
-app.include_router(jogador_router)
-
-
+    allow_headers=["*"], # Acesso a todos os cabeçalhos
+),
